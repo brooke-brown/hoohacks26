701b1284214e5db3b44b02b3bb1a8e0116019c3b
Brooke Brown <brookebrown2027@outlook.com>
2026-03-21 14:40:14 -0400

    adding BrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login'
 
 export default function App() {
-  return <Login />
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  )
 }
